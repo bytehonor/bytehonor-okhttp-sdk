99f77a40dd14b927a62bcfd097df92268c0ae68d
lijianqiang <bytehonor@126.com>
2019-12-21 23:29:37 +0800

    test

--- src/test/java/com/bytehonor/sdk/basic/okhttp/client/OkHttpBasicClientTest.java
+++ src/test/java/com/bytehonor/sdk/basic/okhttp/client/OkHttpBasicClientTest.java
@@ -16,7 +16,7 @@ public class OkHttpBasicClientTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(OkHttpBasicClientTest.class);
 
-//    @Test
+    @Test
     public void testGetString2() {
         boolean isOk = true;
         try {
@@ -31,17 +31,17 @@ public class OkHttpBasicClientTest {
         assertTrue("*testStartThread*", isOk);
     }
 
-    @Test
+//    @Test
     public void testUpload() {
-        String url = "https://api.weibo.com/2/statuses/share.json";
+        String url = "xx";
 
         boolean isOk = true;
         File file = null;
         try {
-            file = new File("/Users/lijianqiang/Downloads/1.jpg");
+            file = new File("/xxx/1.jpg");
             Map<String, String> paramsMap = new HashMap<String, String>();
-            paramsMap.put("status", "图片微博 https://www.bytehonor.com");
-            paramsMap.put("access_token", "2.00_HdkYHnLI2TE2c1f14a473FclYfC");
+            paramsMap.put("status", "xx");
+            paramsMap.put("access_token", "xx");
 
             String res = OkHttpBasicClient.uploadPic(url, paramsMap, file);
             LOG.info("res:{}", res);
