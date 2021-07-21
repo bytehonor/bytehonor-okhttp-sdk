aa13e6bad76a445f1eddea50052991435b988a81
lijianqiang <newphper@126.com>
2021-07-21 12:07:06 +0800

    test

--- src/test/java/com/bytehonor/sdk/okhttp/bytehonor/client/BytehonorOkHttpClientTest.java
+++ src/test/java/com/bytehonor/sdk/okhttp/bytehonor/client/BytehonorOkHttpClientTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -29,6 +33,37 @@ public class BytehonorOkHttpClientTest {
         }
 
         assertTrue("*testStartThread*", isOk);
+    }
+
+    public static void main(String[] arg) {
+        ExecutorService service = Executors.newFixedThreadPool(64);
+        int size = 256;
+        CountDownLatch countDownLatch = new CountDownLatch(size);
+        AtomicInteger ai = new AtomicInteger(0);
+        for (int i = 0; i < size; i++) {
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        // 测测header是否是浏览器的
+                        BytehonorOkHttpClient.get("https://www.baidu.com");
+                        ai.incrementAndGet();
+                        countDownLatch.countDown();
+                    } catch (BytehonorOkhttpSdkException e) {
+                        LOG.error("xxxx error:{}", e.getMessage());
+                    }
+                }
+            });
+        }
+        try {
+            countDownLatch.await();
+            Thread.sleep(5000L);
+        } catch (InterruptedException e) {
+            LOG.info("sleep", e);
+        }
+        int success = ai.intValue();
+        LOG.info("success:{}", success);
+//        assertTrue("*testThread*", size == success);
     }
 
 //    @Test
